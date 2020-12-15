df4438f8dd8af85d385f1cdfff1862e2445f4493
Kitsori <aleex16sanchez@gmail.com>
2020-12-15 09:58:29 +0100

    creacion consulta

--- src/objetosDB/UserQuery.java
+++ src/objetosDB/UserQuery.java
@@ -1,5 +1,25 @@
 package objetosDB;
 
-public class UserQuery {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserQuery extends Conexion{
+	
+	private PreparedStatement ps = null;
+	private Connection con = getConexion();
+	
+	public boolean checkEmail(User user){
+		String sql = "IF EXISTS (SELECT * from user where Email = '" + user.getEmail() + "')";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.executeQuery();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
 
 }
