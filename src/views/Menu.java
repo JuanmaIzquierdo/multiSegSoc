26f6af0128da9febf995d9ce29d8e3baa37b9044
juanma <jmizquierdo@outlook.es>
2020-12-16 11:36:18 +0100

    Directorios ftp

--- src/views/Menu.java
+++ src/views/Menu.java
@@ -32,30 +32,35 @@ public class Menu extends JFrame {
 	static Menu frame;
 	JPanel panelFile;
 	JPanel panelMenu;
+	JFileChooser fc ;
 	MenuController controller;
 
 	/**
 	 * Launch the application.
 	 */
 	// constructor que pida por parametro un menuController*****************+
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					frame = new Menu();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
 	 */
-	public Menu(MenuController controller) {
-		this.controller = controller;
+	 public Menu() {
+		// TODO Auto-generated constructor stub
+	//}
+	//public Menu(MenuController controller) {
+		//this.controller = controller;
+		 fc= new JFileChooser();
 		setBackground(UIManager.getColor("Button.shadow"));
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -73,10 +78,11 @@ public class Menu extends JFrame {
 		JMenu mnNewMenu = new JMenu("Archivos");
 		mnNewMenu.setForeground(new Color(255, 255, 255));
 		menuBar.add(mnNewMenu);
-		JMenuItem mntmNuevoArchivo = new JMenuItem("Nuevo Archivo");
+		JMenuItem mntmNuevoArchivo = new JMenuItem("Subir Archivo");
 		mntmNuevoArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {								
 				String boton = "subir";
+				fc.setCurrentDirectory(new File(System.getProperty("user.home") + System.getProperty("file.separator")+ "Documentos"));
 				menuFilechooser(boton);			
 			}
 		});
@@ -88,6 +94,7 @@ public class Menu extends JFrame {
 		JMenuItem mntmBorrarArchivo = new JMenuItem("Borrar Archivo");
 		mntmBorrarArchivo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
 				String boton = "borrar";				
 				menuFilechooser(boton);
 			}
@@ -113,19 +120,6 @@ public class Menu extends JFrame {
 		mnEmail.setForeground(new Color(255, 255, 255));
 		menuBar.add(mnEmail);
 
-		JMenu mnRegistro = new JMenu("Registro");
-
-		mnRegistro.setForeground(new Color(255, 255, 255));
-		menuBar.add(mnRegistro);
-
-		JMenuItem mntmNuevoUsuario = new JMenuItem("Nuevo Usuario");
-		mntmNuevoUsuario.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				menuRegistro();
-			}
-		});
-		mnRegistro.add(mntmNuevoUsuario);
-
 		JMenu mnAcercaDe = new JMenu("Acerca de");
 		mnAcercaDe.setForeground(new Color(255, 255, 255));
 		menuBar.add(mnAcercaDe);
@@ -139,12 +133,13 @@ public class Menu extends JFrame {
 
 	public void menuFilechooser(String boton) {	
 		vaciarVentana();
+		
 		panelFile = new JPanel();
 		contentPane.updateUI();
 		panelFile.setBounds(0, 27, 677, 376);
 		contentPane.add(panelFile);
 		// Creamos el objeto JFileChooser
-		JFileChooser fc = new JFileChooser();
+		
 		// Abrimos la ventana, guardamos la opcion seleccionada por el usuario
 		panelFile.add(fc);
 		JButton btnNewButton = new JButton(boton);
@@ -158,81 +153,6 @@ public class Menu extends JFrame {
 		panelFile.add(btnNewButton);
 		panelFile.setVisible(false);
 		panelFile.setVisible(true);
-	}
-
-	public void menuRegistro() {
-		vaciarVentana();
-		panelMenu = new JPanel();
-		panelMenu.revalidate();
-		panelMenu.setBackground(new Color(192, 192, 192));
-		panelMenu.setBounds(0, 23, 677, 380);
-		contentPane.add(panelMenu);
-		panelMenu.setLayout(null);
-
-		JLabel label = new JLabel("");
-		label.setBounds(338, 5, 0, 0);
-		panelMenu.add(label);
-
-		JLabel lblNewLabel = new JLabel("Nombre");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setBounds(113, 34, 150, 16);
-		panelMenu.add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("Apellido");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setBounds(113, 88, 150, 16);
-		panelMenu.add(lblNewLabel_1);
-
-		JLabel lblNewLabel_2 = new JLabel("Email");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_2.setForeground(new Color(255, 255, 255));
-		lblNewLabel_2.setBounds(113, 142, 150, 16);
-		panelMenu.add(lblNewLabel_2);
-
-		JLabel lblNewLabel_3 = new JLabel("Contrase\u00F1a");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_3.setForeground(new Color(255, 255, 255));
-		lblNewLabel_3.setBounds(113, 196, 150, 16);
-		panelMenu.add(lblNewLabel_3);
-
-		JLabel lblNewLabel_4 = new JLabel("Repita contrase\u00F1a");
-		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel_4.setForeground(new Color(255, 255, 255));
-		lblNewLabel_4.setBounds(113, 250, 150, 16);
-		panelMenu.add(lblNewLabel_4);
-
-		textField = new JTextField();
-		textField.setBounds(275, 34, 273, 22);
-		panelMenu.add(textField);
-		textField.setColumns(10);
-
-		textField_1 = new JTextField();
-		textField_1.setBounds(275, 89, 273, 22);
-		panelMenu.add(textField_1);
-		textField_1.setColumns(10);
-
-		textField_2 = new JTextField();
-		textField_2.setBounds(275, 144, 273, 22);
-		panelMenu.add(textField_2);
-		textField_2.setColumns(10);
-
-		textField_3 = new JTextField();
-		textField_3.setBounds(275, 199, 273, 22);
-		panelMenu.add(textField_3);
-		textField_3.setColumns(10);
-
-		textField_4 = new JTextField();
-		textField_4.setBounds(275, 254, 273, 22);
-		panelMenu.add(textField_4);
-		textField_4.setColumns(10);
-
-		JButton btnRegistrar = new JButton("Registrar");
-		btnRegistrar.setBounds(290, 314, 97, 25);
-		panelMenu.add(btnRegistrar);
-		contentPane.setVisible(false);
-		contentPane.setVisible(true);
 	}
 	
 	public void vaciarVentana() {
