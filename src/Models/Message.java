63358c01f3325149086069557e75e6bee3728102
Pablo Mérida <pablomerida2001@gmail.com>
2021-01-10 14:15:49 +0100

    crear modelos para mandar mensajes a servidor

---
+++ src/Models/Message.java
@@ -0,0 +1,48 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class Message {
+	
+	private String code;
+	private Boolean error;
+	private String errorMsg;
+	private ArrayList<Object> data;
+	
+	public Message(String code) {
+		this.code = code;
+		this.data = new ArrayList<Object>();
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Boolean getError() {
+		return error;
+	}
+
+	public void setError(Boolean error) {
+		this.error = error;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+
+	public ArrayList<Object> getData() {
+		return data;
+	}
+
+	public void setData(ArrayList<Object> data) {
+		this.data = data;
+	}
+}
