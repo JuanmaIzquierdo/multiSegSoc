e13cc52b71716780cf15421e318fcfd969cf0e07
Pablo <Pablo@DESKTOP-VFSN8EJ>
2021-01-15 19:50:41 +0100

    a

--- src/controller/FtpController.java
+++ src/controller/FtpController.java
@@ -1,14 +1,5 @@
 package controller;
-/*
- * Clase FtpController
- * 
- * Clase que gestiona el FTP Server
- * 
- * @Author Grupo2
- * 
- * @Version 1.0
- * 
- */
+
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
@@ -26,46 +17,29 @@ import Models.FtpResponse;
 
 public class FtpController {
 	
-	private String username;
-	private String password;
-	private FTPClient client;
-	private String homeDirectory;
+	String username;
+	String password;
+	FTPClient client;
+	String homeDirectory;
 	
-	/**
-     * Constructor con 4 parametros
-     * @param username - type String - nombre del usuario
-     * @param password - type String - contraseña del empleado
-     */
 	public FtpController(String username, String password) {
 		this.username = username;
 		this.password = password;
 		this.client  = new FTPClient();
 	}
 	
-	/**
-     * Cenecion al servidor
-     * @return
-	     *  true: se ha conectado
-	     *  false: error durante la conecion<
-     */
 	public boolean connect() {
 		try {
-			client.connect("192.168.1.68");
+			client.connect("localhost");
 			return true;
 		} catch (SocketException e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			e.printStackTrace();
 		}
 		return false;
 	}
 	
-	/**
-     * Login in servidor
-     * @return
-	     *  true: se ha hecho login correctamente
-	     *  false: error durante el proceso
-     */
 	public boolean loginFTP() {
 		 try {
 			if(client.login(username, password)){
@@ -94,14 +68,6 @@ public class FtpController {
 		}
 	}
 
-	/**
-     * Subir el archivo
-     * @param path - type String - la ruta del archivi
-     * @param name - type String - el nombre del fichero
-     * @return
-	     *  true: se ha subido correctamente
-	     *  false: error durante el proceso
-     */
 	public boolean uploadFile(String path, String name) {
 		try {
 			BufferedInputStream in = new BufferedInputStream(new FileInputStream(path));
@@ -113,15 +79,6 @@ public class FtpController {
 		}
 	}
 
-	/**
-     * Bajar in servidor
-     * @param path - type String - la ruta del archivi
-     * @param fileName - type String - el nombre del fichero
-     * @param localDirectory - type String - el nombre del directorio
-     * @return
-	     *  true: se ha hecho login correctamente
-	     *  false: error durante el proceso
-     */
 	public boolean downloadFile(String path, String localDirectory, String fileName) {
 		BufferedOutputStream out;
 		try {
@@ -134,11 +91,6 @@ public class FtpController {
 		}
 	}
 	
-	/**
-     * Borar archivo/directorio del servidor
-     * @param path - type String - la ruta del archivo/directorio
-     * @return FtpResponse - respuesta
-     */
 	public FtpResponse deleteFile(String path) {
 		try {
 			if(!client.deleteFile(path)) {
@@ -153,13 +105,6 @@ public class FtpController {
 		}
 	}
 	
-	 /* Borar directorio del servidor (si los directorios no estan vacios)
-     * @param path - type String - la ruta del directorio
-     * @param currentDir - type String - se uteliza cuando se borra recursivamente para saber en que directorio esta
-     * @return
-	     *  true: se ha borrado correctamente
-	     *  false: error durante el proceso
-     */
 		public boolean deleteDirectory(String parentDir, String currentDir) throws IOException {
 			boolean result = false;
 			String dirToList = parentDir;
@@ -169,15 +114,25 @@ public class FtpController {
 	            dirToList += "/" + currentDir;
 
 	        }
+
+	 
+
 	        FTPFile[] subFiles = client.listFiles(dirToList);
+
+	 
 
 	        if (subFiles != null && subFiles.length > 0) {
 
 	            for (FTPFile aFile : subFiles) {
+
 	                String currentFileName = aFile.getName();
+
 	                if (currentFileName.equals(".") || currentFileName.equals("..")) {
+
 	                    // skip parent directory and the directory itself
+
 	                    continue;
+
 	                }
 
 	                String filePath = parentDir + "/" + currentDir + "/"
@@ -185,19 +140,31 @@ public class FtpController {
 	                        + currentFileName;
 
 	                if (currentDir.equals("")) {
+
 	                    filePath = parentDir + "/" + currentFileName;
+
 	                }
 
+	 
+
 	                if (aFile.isDirectory()) {
+
 	                    // remove the sub directory
+
 	                    deleteDirectory(dirToList, currentFileName);
+
 	                } else {
+
 	                    // delete the file
 
 	                    boolean deleted = client.deleteFile(filePath);
+
 	                    if (deleted) {
+
 	                        System.out.println("DELETED the file: " + filePath);
+
 	                    } else {
+
 	                        System.out.println("CANNOT delete the file: "
 
 	                                + filePath);
@@ -216,13 +183,6 @@ public class FtpController {
 	   	 return result;
 	}
 	
-		/* Renombrar archivo/directorio del servidor
-	     * @param path - type String - la ruta del directorio
-	     * @param newName - type String - nuevo nombre
-     * @return
-	     *  true: se ha renombrado correctamente
-	     *  false: error durante el proceso
-	     */
 	public boolean renameFile(String path, String newName) {
 		try {
 			client.rename(path, newName);
@@ -233,12 +193,6 @@ public class FtpController {
 		}
 	}
 	
-	/* crear directorio del servidor
-     * @param path - type String - la ruta del directorio
-     * @return
-	     *  true: se ha creado correctamente
-	     *  false: error durante el proceso
-     */
 	public boolean createDirectory(String path) {
         try {
             client.makeDirectory(path);
@@ -249,17 +203,10 @@ public class FtpController {
         }
     }
 	
-	/* Coge el directorio raiz (del usuario)
-     * @return String - ruta del directorio
-     */
 	public String getHomeDirectory() {
 		return this.homeDirectory;
 	}
 	
-	/* Coge los archivos del directorio
-     * @param folder - type String - la ruta del directorio
-     * @return FTPFile[] - archivos
-     */
 	public FTPFile[] getDirectoryFiles(String folder) {
 		try {
 			String directory = this.homeDirectory+folder;
